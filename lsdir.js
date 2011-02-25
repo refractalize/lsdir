4f7de4a7585dcf049f9e3e7393fb8be04247634e
Tim Macfarlane <timmacfarlane@gmail.com>
2011-02-25 08:40:27 +0000

    for result

--- lsdir.js
+++ lsdir.js
@@ -1,11 +1,36 @@
 var fs = require('fs');
 var _ = require('underscore');
 
+var callbacker = function () {
+    var _this = _(arguments).head();
+    var func_args = _(arguments).tail();
+    var func = _(func_args).head();
+    var args = _(func_args).tail();
+
+    var result, error;
+
+    args.push(function (err, res) {
+        if (result) result(res);
+        if (error) error(err);
+    });
+
+    func.apply(_this, args);
+
+    return {
+        result: function (f) {
+            result = f;
+        },
+        error: function (f) {
+            error = f;
+        }
+    };
+}
+
 var getDirectories = function (receiveDirectories) {
     var dirs = [];
     var n = 0;
 
-    fs.readdir('.', function (err, files) {
+    callbacker(fs, fs.readdir, '.').result(function (files) {
         _(files).each(function (file) {
             n++;
             fs.stat(file, function (err, fileStat) {
