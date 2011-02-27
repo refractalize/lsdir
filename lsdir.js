af968e2904fa9ded64bf20537e4921c0b82a9d9b
Tim Macfarlane <timmacfarlane@gmail.com>
2011-02-27 12:00:13 +0000

    moved zo out to own module

--- lsdir.js
+++ lsdir.js
@@ -1,47 +1,6 @@
 var fs = require('fs');
 var _ = require('underscore');
-
-var zo = function (items, pipeline) {
-    pipeline = (pipeline || []);
-
-    var runPipeline = function (items, funcs) {
-        first = _(funcs).head();
-        if (first) {
-            first(items, function (it) { runPipeline(it, _(funcs).tail()); });
-        }
-    }
-
-    return {
-        results: function (f) {
-            pipeline.push(function (items, next) {
-                f(items);
-            });
-            runPipeline(items, pipeline);
-        },
-        select: function (selector) {
-            pipeline.push(function (items, next) {
-                var n = items.length;
-                var selectedItems = [];
-
-                if (n > 0) {
-                    _(items).each(function (item) {
-                        selector(item, function (selected) {
-                            if (selected) selectedItems.push(item);
-                            n--;
-                            if (n == 0) {
-                                next(selectedItems);
-                            }
-                        });
-                    });
-                } else {
-                    next(selectedItems);
-                }
-            });
-
-            return zo(items, pipeline);
-        }
-    };
-};
+var zo = require('./zo').zo;
 
 var getDirectories = function (receiveDirectories) {
     fs.readdir('.', function (err, files) {
