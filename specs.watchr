f0cf693f0adc935a5f9b85edb1b9c095179972ed
Tim Macfarlane <timmacfarlane@gmail.com>
2011-02-27 13:09:54 +0000

    added map

--- specs.watchr
+++ specs.watchr
@@ -2,10 +2,16 @@ def get_spec(filename)
     if filename =~ /_spec\.rb$/
         filename
     else
-        File.basename(filename, File.extname(filename)) + '_spec.rb'
+        file = File.basename(filename, File.extname(filename)) + '_spec.rb'
+        if File.exist?(file)
+            file
+        end
     end
 end
 
 watch '.*\.(rb|js)' do |filename|
-    system("clear; rspec #{get_spec(filename[0])} --color")
+    spec_file = get_spec(filename[0])
+    if spec_file
+        system("clear; rspec #{spec_file} --color")
+    end
 end
--- zo.js
+++ zo.js
@@ -1,47 +1,60 @@
 var _ = require('underscore');
 
-(function () {
-    var zo = function (items, pipeline) {
-        pipeline = (pipeline || []);
+var zo = function (items, pipeline) {
+    pipeline = (pipeline || []);
 
-        var runPipeline = function (items, funcs) {
-            first = _(funcs).head();
-            if (first) {
-                first(items, function (it) { runPipeline(it, _(funcs).tail()); });
-            }
+    var runPipeline = function (items, funcs) {
+        first = _(funcs).head();
+        if (first) {
+            first(items, function (it) { runPipeline(it, _(funcs).tail()); });
         }
+    }
 
-        return {
-            results: function (f) {
-                pipeline.push(function (items, next) {
-                    f(items);
-                });
-                runPipeline(items, pipeline);
-            },
-            select: function (selector) {
-                pipeline.push(function (items, next) {
-                    var n = items.length;
-                    var selectedItems = [];
-
-                    if (n > 0) {
-                        _(items).each(function (item) {
-                            selector(item, function (selected) {
-                                if (selected) selectedItems.push(item);
-                                n--;
-                                if (n == 0) {
-                                    next(selectedItems);
-                                }
-                            });
-                        });
-                    } else {
-                        next(selectedItems);
-                    }
-                });
-
-                return zo(items, pipeline);
-            }
-        };
+    var newPipelineElement = function (pipelineElement) {
+        pipeline.push(pipelineElement);
+        return zo(items, pipeline);
     };
 
-    exports.zo = zo;
-}());
+    var pipelineElement = function (processItem, addItem) {
+        pipeline.push(function (items, next) {
+            var n = items.length;
+            var processedItems = [];
+
+            if (n > 0) {
+                _(items).each(function (item) {
+                    processItem(item, function (processedItem) {
+                        addItem(processedItems, item, processedItem);
+                        n--;
+                        if (n == 0) {
+                            next(processedItems);
+                        }
+                    });
+                });
+            } else {
+                next(processedItems);
+            }
+        });
+        return zo(items, pipeline);
+    };
+
+    return {
+        results: function (f) {
+            pipeline.push(function (items, next) {
+                f(items);
+            });
+            runPipeline(items, pipeline);
+        },
+        map: function (mapper) {
+            return pipelineElement(mapper, function (mappedItems, item, mappedItem) {
+                mappedItems.push(mappedItem);
+            });
+        },
+        select: function (selector) {
+            return pipelineElement(selector, function (selectedItems, item, itemSelected) {
+                if (itemSelected) selectedItems.push(item);
+            });
+        }
+    };
+};
+
+exports.zo = zo;
